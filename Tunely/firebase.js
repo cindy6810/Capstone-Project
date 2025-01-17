8260c5a9356794f54d5b4dc6a44cb308f753e892
Malek Mansour <malek24mansour@gmail.com>
2025-01-17 09:39:23 -0700

    Update firebase.js

--- Tunely/firebase.js
+++ Tunely/firebase.js
@@ -1,6 +1,5 @@
 import firebase from "firebase/app";
-import "firebase/firestore"; // Import Firestore if needed
-import { firebaseConfig } from "./firebaseConfig"; // Your Firebase configuration file
+import { firebaseConfig } from "firebaseConfig"; 
 
 // Initialize Firebase
 if (!firebase.apps?.length) {
