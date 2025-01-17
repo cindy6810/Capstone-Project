48807ff4004de1ddcf3aa77fc9b732d06d32d21a
Malek Mansour <malek24mansour@gmail.com>
2025-01-17 11:43:34 -0700

    Update firebaseConfig.js

--- Tunely/firebaseConfig.js
+++ Tunely/firebaseConfig.js
@@ -1,21 +1,15 @@
-// Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
 
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyDS7Ce-Rvap_8keSJ5Y3fEruwCuujShGBU",
-  authDomain: "tunely-111.firebaseapp.com",
-  projectId: "tunely-111",
-  storageBucket: "tunely-111.firebasestorage.app",
-  messagingSenderId: "209540280192",
-  appId: "1:209540280192:web:e7b8139efd06deeddf1b65",
-  measurementId: "G-B6D765XB6G"
+  apiKey: process.env.FIREBASE_API_KEY,
+  authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.FIREBASE_PROJECT_ID,
+  storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.FIREBASE_APP_ID,
+  measurementId: process.env.FIREBASE_MEASUREMENT_ID,
 };
 
-// Initialize Firebase
 const app = initializeApp(firebaseConfig);
 const analytics = getAnalytics(app);
