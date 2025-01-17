98568d9b4d5d06fd285b8888ff5f2541e976d893
Malek Mansour <malek24mansour@gmail.com>
2025-01-17 09:15:29 -0700

    Update firebaseConfig.js

--- Tunely/firebaseConfig.js
+++ Tunely/firebaseConfig.js
@@ -1,10 +1,21 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-    apiKey: "your-api-key",
-    authDomain: "your-project-id.firebaseapp.com",
-    projectId: "your-project-id",
-    storageBucket: "your-project-id.appspot.com",
-    messagingSenderId: "your-messaging-sender-id",
-    appId: "your-app-id",
-    measurementId: "your-measurement-id" // Optional
-  };
-  
+  apiKey: "AIzaSyDS7Ce-Rvap_8keSJ5Y3fEruwCuujShGBU",
+  authDomain: "tunely-111.firebaseapp.com",
+  projectId: "tunely-111",
+  storageBucket: "tunely-111.firebasestorage.app",
+  messagingSenderId: "209540280192",
+  appId: "1:209540280192:web:3dea184079a814a6df1b65",
+  measurementId: "G-FXYG5NPNBK"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
