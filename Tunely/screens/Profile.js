e5dd2f23f3818d7cb2c242633a9a6d9a822aebc7
Malek Mansour <malek24mansour@gmail.com>
2025-01-21 15:28:31 -0700

    Add Profile.js

---
+++ Tunely/screens/Profile.js
@@ -0,0 +1,24 @@
+import React from "react";
+import { View, Text, StyleSheet } from "react-native";
+
+export default function ProfileScreen() {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.title}>Profile Page</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: "#F0F0D7",
+  },
+  title: {
+    fontSize: 24,
+    fontWeight: "bold",
+    color: "#727D73",
+  },
+});
