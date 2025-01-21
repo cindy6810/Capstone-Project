d5b94d4e207267bc386315df6f862a55668c9997
Malek Mansour <malek24mansour@gmail.com>
2025-01-21 15:17:30 -0700

    Delete firebase.js

--- Tunely/back-end/firebase.js
+++
@@ -1,9 +0,0 @@
-import firebase from "firebase/app";
-import { firebaseConfig } from "firebaseConfig"; 
-
-// Initialize Firebase
-if (!firebase.apps?.length) {
-  firebase.initializeApp(firebaseConfig);
-}
-
-export default firebase;
