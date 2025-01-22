c76f5a94a26c3809307564a423fd15799966aae7
Kaleb Berhane <kaleb.cpu@gmail.com>
2025-01-22 11:44:33 -0700

    fixed style

--- Tunely/screens/Home.js
+++ Tunely/screens/Home.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { Text, View, FlatList, TouchableOpacity } from "react-native";
-import { styles } from "./styles";
+import { styles } from "../styles";
 
 export default function HomeScreen() {
   return (
