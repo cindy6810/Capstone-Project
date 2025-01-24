1f5248421e3cf3fc3ef667def6f7eedc96b4c7ec
Malek Mansour <malek24mansour@gmail.com>
2025-01-23 19:15:29 -0700

    Update firebaseConfig.js

--- Tunely/Utility/firebaseConfig.js
+++ Tunely/Utility/firebaseConfig.js
@@ -1,6 +1,5 @@
 import { initializeApp } from 'firebase/app';
-import { getAuth, signInWithCredential, GoogleAuthProvider } from 'firebase/auth';
-import { GoogleSignin } from '@react-native-google-signin/google-signin';
+import { getAuth, signInWithCredential } from 'firebase/auth';
 
 const firebaseConfig = {
   apiKey: "AIzaSyDS7Ce-Rvap_8keSJ5Y3fEruwCuujShGBU",
@@ -16,26 +15,5 @@ const firebaseConfig = {
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 const auth = getAuth(app);
-
-// Configure Google Sign-In
-GoogleSignin.configure({
-  webClientId: '209540280192-cfv5dseulqeeq95t9vagrhmbt2acfva4.apps.googleusercontent.com', // Use your web client ID
-});
-
-// Function for Google Sign-In
-export const signInWithGoogle = async () => {
-  try {
-    // Get user information from Google Sign-In
-    await GoogleSignin.hasPlayServices();
-    const userInfo = await GoogleSignin.signIn();
-    const googleCredential = GoogleAuthProvider.credential(userInfo.idToken);
-    
-    // Sign in to Firebase with the Google credential
-    const userCredential = await signInWithCredential(auth, googleCredential);
-    return userCredential.user; // Return the signed-in user
-  } catch (error) {
-    throw new Error(error.message);
-  }
-};
 
 export default app;
