60a20f451a5960b8b6706652183b2a8eec732f9f
Malek Mansour <malek24mansour@gmail.com>
2025-01-23 20:36:40 -0700

    Update firebaseConfig.js

--- Tunely/Utility/firebaseConfig.js
+++ Tunely/Utility/firebaseConfig.js
@@ -15,6 +15,7 @@ const firebaseConfig = {
 };
 
 // Initialize Firebase
+// Initialize Firebase
 const app = initializeApp(firebaseConfig);
 const auth = getAuth(app);
 const database = getDatabase(app);
