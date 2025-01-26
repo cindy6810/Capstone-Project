8d4406e5b9afb5b9fc9a8deca1062f05863675e6
Malek Mansour <138655005+MalekMansour@users.noreply.github.com>
2025-01-26 15:38:00 -0700

    Update Settings.js

--- Tunely/screens/Settings.js
+++ Tunely/screens/Settings.js
@@ -6,7 +6,6 @@ import { Ionicons } from "@expo/vector-icons";
 export default function SettingsScreen({ navigation }) {
   return (
     <View style={styles.container}>
-      {/* Back Button */}
       <TouchableOpacity style={{ marginBottom: 80 , top: 50 }} onPress={() => navigation.goBack()}>
         <Ionicons name="arrow-back" size={24} color="#f1f1f1" />
       </TouchableOpacity>
