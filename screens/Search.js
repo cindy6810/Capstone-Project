7f5897a539a2345f811f0c6f286e267c2f01cfd2
Malek Mansour <malek24mansour@gmail.com>
2025-02-08 14:38:05 -0700

    Update Search.js

--- screens/Search.js
+++ screens/Search.js
@@ -1,15 +1,8 @@
 import React from "react";
-import { TextInput, View, FlatList } from "react-native";
+import { TextInput, View, FlatList, TouchableOpacity, Text } from "react-native";
 import { styles } from "../styles";
-import SongCard from "../components/SongCard";
 
 export default function SearchScreen() {
-
-  const searchResults = [
-    { id: "1", title: "Search Result 1", artist: "Artist 1", image: require("../assets/note.jpg") },
-    { id: "2", title: "Search Result 2", artist: "Artist 2", image: require("../assets/note.jpg") },
-  ];
-
   return (
     <View style={styles.container}>
       <TextInput
@@ -17,11 +10,18 @@ export default function SearchScreen() {
         placeholder="Search song, artist, album..."
         placeholderTextColor="#E14594"
       />
-      
+      X
       <FlatList
-        data={searchResults}
+        data={[
+          { id: "1", title: "Search Result 1" },
+          { id: "2", title: "Search Result 2" },
+        ]}
         keyExtractor={(item) => item.id}
-        renderItem={({ item }) => <SongCard song={item} />}
+        renderItem={({ item }) => (
+          <TouchableOpacity style={styles.songCard}>
+            <Text style={styles.songTitle}>{item.title}</Text>
+          </TouchableOpacity>
+        )}
       />
     </View>
   );
